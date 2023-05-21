28b5d973284fbd2b1fdaad8e853e9585ebc3ecda
Ischquack <isakheim@gmail.com>
2023-05-21 11:27:04 +0200

    testing

--- components/chatGPT/test/ChatGptPlugin.test.tsx
+++ components/chatGPT/test/ChatGptPlugin.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import fireEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { ThemeProvider, studioTheme } from '@sanity/ui';
 import ChatGptPlugin from '../ChatGptPlugin';
